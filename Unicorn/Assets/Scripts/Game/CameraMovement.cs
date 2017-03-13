b6b7e38c20bacfa721667d269afe3243375199e9
Peter Zhou <peterzhou43@gmail.com>
2017-03-12 17:42:21 -0700

    no message

---
+++ Unicorn/Assets/Scripts/Game/CameraMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+	public GameObject followTarget;
+
+	public float moveSpeed;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (followTarget != null) 
+		{
+				transform.position = Vector3.Lerp (transform.position,
+					followTarget.transform.position, Time.deltaTime * moveSpeed);
+		}
+	}
+}
---
+++ Unicorn/Assets/Scripts/Game/UnicornMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnicornMovement : MonoBehaviour {
+
+    Animator anim;
+    Rigidbody rb;
+    int jumpHash = Animator.StringToHash("Jump");
+    float move = 0.0f;
+    int walkStateHash = Animator.StringToHash("Base Layer.Walk");
+    int runStateHash = Animator.StringToHash("Base Layer.Run");
+    int jumpStateHash = Animator.StringToHash("Base Layer.Jump");
+    float runSecounds = 0.0f;
+    float unicornAcc = 1.0f;
+    float maxBonusSpeedRate = 1.0f;
+    float normalTopSpeed = 4.0f;
+    float maxBonusSpeed = 0;
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        maxBonusSpeed = maxBonusSpeedRate * normalTopSpeed;
+    }
+
+    void FixedUpdate()
+    {
+    }
+    // Update is called once per frame
+    void Update () {
+        move = Input.GetAxis("Vertical");
+
+        if (move == 1)
+        {
+            runSecounds = runSecounds + Time.deltaTime;
+        }
+        else
+        {
+            runSecounds = 0;
+        }
+
+        anim.SetFloat("Speed", move);
+        float bonusSpeed = (runSecounds / unicornAcc);
+        float walkSpeed = 0.25f * normalTopSpeed;
+        float runSpeed = move * 0.75f * normalTopSpeed;
+        
+        if (bonusSpeed > maxBonusSpeed)
+        {
+            bonusSpeed = maxBonusSpeed;
+        }
+
+        float currentSpeed = walkSpeed + runSpeed + bonusSpeed;
+
+        anim.SetFloat("RunSpeed", 1 + bonusSpeed / normalTopSpeed);
+
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + currentSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+            if (stateInfo.fullPathHash != jumpStateHash )
+            {
+                anim.SetTrigger(jumpHash);
+                Vector3 jumpSpeed = new Vector3(0, 6f, currentSpeed);
+                rb.velocity = rb.velocity + jumpSpeed;
+            }
+        }
+    }
+}
